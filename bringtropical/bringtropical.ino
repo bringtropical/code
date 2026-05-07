d3bd57115e9d495229bf634a83126d3046ee5cfa
Prankapple <bnnaadcscs@gmail.com>
2026-05-07 17:39:56 +0300

    Implement moisture sensor for automated watering
    Added moisture sensor functionality to control watering based on soil moisture levels. Integrated serial output for dry/wet state detection.

--- bringtropical/bringtropical.ino
+++ bringtropical/bringtropical.ino
@@ -1,29 +1,51 @@
+int sensorPin = A0;
 const int buttonPin = 2;
 const int relayPin = 8;
 
-int lastButtonState = HIGH;
-
 const unsigned long relayOnTime = 1000;
 
+bool wasWet = true;
+
+void triggerRelay();
+
 void setup() {
+  Serial.begin(9600);
+
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(relayPin, OUTPUT);
 
   digitalWrite(relayPin, HIGH);
 }
 
+void loop() {
+
+  int moisture = analogRead(sensorPin);
+
+  if (moisture > 700) {
+
+    if (wasWet) {
+      Serial.println("dry detected -> watering once");
+      triggerRelay();
+      wasWet = false;
+    } else {
+      Serial.println("still dry -> no repeat");
+    }
+
+  } else {
+    Serial.println("wet -> reset state");
+    wasWet = true;
+  }
+
+  if (digitalRead(buttonPin) == LOW) {
+    triggerRelay();
+    delay(300);
+  }
+
+  delay(20);
+}
+
 void triggerRelay() {
   digitalWrite(relayPin, LOW);
   delay(relayOnTime);
   digitalWrite(relayPin, HIGH);
-}
-
-void loop() {
-  int reading = digitalRead(buttonPin);
-
-  if (lastButtonState == HIGH && reading == LOW) {
-    triggerRelay();
-  }
-
-  lastButtonState = reading;
 }
