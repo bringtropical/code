22ad679c058c7a7ee8249fb27ef0597f9f424617
Prankapple <bnnaadcscs@gmail.com>
2026-05-07 16:02:05 +0300

    Update bringtropical.ino

--- bringtropical/bringtropical.ino
+++ bringtropical/bringtropical.ino
@@ -3,37 +3,27 @@ const int relayPin = 8;
 
 int lastButtonState = HIGH;
 
-// timing
-const unsigned long interval = 86400000UL; // 24 hours
-const unsigned long relayOnTime = 1000;    // 1 second
-unsigned long lastAutoTrigger = 0;
+const unsigned long relayOnTime = 1000;
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(relayPin, OUTPUT);
 
-  digitalWrite(relayPin, HIGH); // relay OFF (active LOW)
+  digitalWrite(relayPin, HIGH);
 }
 
 void triggerRelay() {
-  digitalWrite(relayPin, LOW);  // ON
+  digitalWrite(relayPin, LOW);
   delay(relayOnTime);
-  digitalWrite(relayPin, HIGH); // OFF
+  digitalWrite(relayPin, HIGH);
 }
 
 void loop() {
-  unsigned long currentTime = millis();
-
-  // 🔘 BUTTON TRIGGER
   int reading = digitalRead(buttonPin);
+
   if (lastButtonState == HIGH && reading == LOW) {
     triggerRelay();
   }
-  lastButtonState = reading;
 
-  // ⏰ AUTO DAILY TRIGGER
-  if (currentTime - lastAutoTrigger >= interval) {
-    lastAutoTrigger = currentTime;
-    triggerRelay();
-  }
+  lastButtonState = reading;
 }
